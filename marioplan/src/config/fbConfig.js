1fdd4971ff5fea2bd8b464d52a097d04484faa20
Ishwant Singh <ishwant8@gmail.com>
2019-03-24 14:28:33 +0530

    23/40 done

--- marioplan/src/config/fbConfig.js
+++ marioplan/src/config/fbConfig.js
@@ -1,17 +1,17 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 // Replace this with your own config details
 var config = {
-  apiKey: "AIzaSyDo2HauU6PtL3PY6KthXdpIUzbDV908avo",
-  authDomain: "marioplan-files.firebaseapp.com",
-  databaseURL: "https://marioplan-files.firebaseio.com",
-  projectId: "marioplan-files",
-  storageBucket: "marioplan-files.appspot.com",
-  messagingSenderId: "1011174152626"
+  apiKey: "AIzaSyDfCxz2evQ4wiBA8V6kTeqQLyUJWCHBMoA",
+  authDomain: "plan-it-88.firebaseapp.com",
+  databaseURL: "https://plan-it-88.firebaseio.com",
+  projectId: "plan-it-88",
+  storageBucket: "plan-it-88.appspot.com",
+  messagingSenderId: "974410145183"
 };
 firebase.initializeApp(config);
 firebase.firestore().settings({ timestampsInSnapshots: true });
 
-export default firebase 
+export default firebase;
